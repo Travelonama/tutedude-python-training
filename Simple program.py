3a5ecae8894cf17a0103feab0d5f0640b7d3a04e
Travelonama <info.travelonama@gmail.com>
2025-05-04 17:34:32 +0530

    Add files via upload

---
+++ Simple program.py
@@ -0,0 +1,20 @@
+# TASK 1
+a=float(input('enter the first number: '))
+b=float(input('enter the second number: '))
+print("addition", a+b)
+print("subtraction", a-b)
+print("multiplication", a*b)
+print("division", a/b)
+
+#TASK 2
+first_name = input('enter your first name; ')
+last_name = input('enter your last name: ')
+full_name = f'{first_name} {last_name}'
+
+print(f'hello {full_name}, happy to see you!')
+
+
+
+
+
+
