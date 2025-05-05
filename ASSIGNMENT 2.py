8630fdf73c4e3adf6122a252def3d981e77bb78d
Travelonama <info.travelonama@gmail.com>
2025-05-05 23:45:58 +0530

    Add files via upload

--- ASSIGNMENT 2.py
+++ ASSIGNMENT 2.py
@@ -1,4 +1,4 @@
-'''
+
 num=int(input('Enter a number:'))
 
 if num % 2 == 0:
@@ -6,15 +6,13 @@ if num % 2 == 0:
 
 else:
     print(f"{num} is an odd number")
-'''
 
-# Initialize a variable to store the sum
+
+
 total_sum = 0
 
-# Iterate over numbers from 1 to 50 (inclusive)
 for number in range(1, 51):
-    total_sum += number  # Add the current number to the total sum
+    total_sum += number
 
-# Display the final sum
 print("The sum of numbers from 1 to 50 is:", total_sum)
 
